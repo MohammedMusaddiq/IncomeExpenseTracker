bb63ac951b51b15af177afda56a631cb459f4047
Mohammed Musaddiq <75469721+MohammedMusaddiq@users.noreply.github.com>
2022-05-09 07:25:02 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,3 @@
-import debug_toolbar
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -8,6 +5,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('', include('iet.urls', namespace='iet', )),
-    path('__debug__/', include(debug_toolbar.urls)),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
